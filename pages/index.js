74066e491620ad8a1143cf30add4b1b0c5dd0573
TJ Mazeika <mazeika.tj@gmail.com>
2021-10-25 21:00:07 -0400

    Fix lint issues

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Index() {
   return (
@@ -7,19 +9,21 @@ export default function Index() {
         <div className="pt-24 pb-48 bg-dither">
           <div className="container mx-auto px-3 filter drop-shadow-md">
             <div className="grid grid-flow-col auto-cols-max items-center gap-x-6">
-              <a href="/">
-                <img className="w-10" src="/logo.svg" alt="foldleft logo"/>
-              </a>
+              <Link href="/">
+                <a>
+                  <Image src="/logo.svg" alt="foldleft logo" width={40} height={30}/>
+                </a>
+              </Link>
               <h1 className="row-start-1 text-2xl font-mono font-bold tracking-wide">
                 foldleft
               </h1>
               <div className="row-start-2 mt-24">
                 <h1 className="text-5xl font-serif font-medium leading-tight">
-                  We're the web dev studio<br/>that gets it done right.
+                  We&apos;re the web dev studio<br/>that gets it done right.
                 </h1>
                 <a href="https://calendly.com/mazeika-llc/new-client">
                   <button className="mt-10 py-3 px-6 bg-yellow hover:shadow-xl hover:bg-yellow-2 rounded font-mono font-bold text-black text-opacity-60">
-                    Let's Meet
+                    Let&apos;s Meet
                   </button>
                 </a>
               </div>
